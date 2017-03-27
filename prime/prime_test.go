50210215bd9e2e2c4b03df144b2fbdb09587c442
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2017-03-27 15:08:13 +0200

    improved prime test

--- prime/prime_test.go
+++ prime/prime_test.go
@@ -10,6 +10,8 @@ var tests = []struct {
     {10, 20, []int{11, 13, 17, 19}},
     {20, 30, []int{23, 29}},
     {95, 99, []int{97}},
+    {1000, 1100, []int{1009, 1013, 1019, 1021, 1031, 1033,
+        1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097}},
 }
 
 func TestGetPrimesSync(t *testing.T) {
@@ -60,13 +62,13 @@ func eqList(a, b []int) bool {
 
 func BenchmarkGetPrimesSync(b *testing.B) {
     for i := 0; i <= b.N; i++ {
-        GetPrimesSync(1, 1000)
+        GetPrimesSync(1000, 2000)
     }
 }
 
 func BenchmarkGetPrimesAsync(b *testing.B) {
     for i := 0; i <= b.N; i++ {
         primes := make(chan int)
-        go GetPrimesAsync(1, 1000, primes)
+        go GetPrimesAsync(1000, 2000, primes)
     }
 }
