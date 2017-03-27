29647d983b0a0a7941f25eb99e71253b606fb7bd
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2017-03-27 15:04:33 +0200

    improved prime test

--- prime/prime_test.go
+++ prime/prime_test.go
@@ -2,42 +2,71 @@ package prime
 
 import "testing"
 
+var tests = []struct {
+    from int
+    to int
+    want []int
+}{
+    {10, 20, []int{11, 13, 17, 19}},
+    {20, 30, []int{23, 29}},
+    {95, 99, []int{97}},
+}
+
 func TestGetPrimesSync(t *testing.T) {
-    primes := GetPrimesSync(10, 20)
-    if primes[0] != 11 {
-        t.Error(`GetPrimesSync(10, 20): 11 not found`)
-    }
-    if primes[1] != 13 {
-        t.Error(`GetPrimesSync(10, 20): 13 not found`)
-    }
-    if primes[2] != 17 {
-        t.Error(`GetPrimesSync(10, 20): 17 not found`)
+    for _, test := range tests {
+        got := GetPrimesSync(test.from, test.to)
+        if !eqList(got, test.want) {
+            t.Errorf("GetPrimeSync(%d, %d) got: %d, want: %d",
+                test.from, test.to, got, test.want)
+        }
     }
 }
 
 func TestGetPrimesAsync(t *testing.T) {
-    primes := make(chan int)
-    go GetPrimesAsync(10, 20, primes)
-    if <-primes != 11 {
-        t.Error(`GetPrimesSync(10, 20): 11 not found`)
+    for _, test := range tests {
+        primes := make(chan int)
+        go GetPrimesAsync(test.from, test.to, primes)
+        got := make([]int, 0)
+        for p := range primes {
+            got = append(got, p)
+        }
+        if !eqList(got, test.want) {
+            t.Errorf("GetPrimeAsync(%d, %d) got: %d, want: %d",
+                test.from, test.to, got, test.want)
+        }
     }
-    if <-primes != 13 {
-        t.Error(`GetPrimesSync(10, 20): 13 not found`)
+}
+
+func eqList(a, b []int) bool {
+    if a == nil && b == nil {
+        return true
     }
-    if <-primes != 17 {
-        t.Error(`GetPrimesSync(10, 20): 17 not found`)
+    if a == nil && b != nil {
+        return false
     }
+    if a != nil && b == nil {
+        return false
+    }
+    if len(a) != len(b) {
+        return false
+    }
+    for i, v := range a {
+        if v != b[i] {
+            return false
+        }
+    }
+    return true
 }
 
 func BenchmarkGetPrimesSync(b *testing.B) {
     for i := 0; i <= b.N; i++ {
-        GetPrimesSync(1, 100)
+        GetPrimesSync(1, 1000)
     }
 }
 
 func BenchmarkGetPrimesAsync(b *testing.B) {
     for i := 0; i <= b.N; i++ {
         primes := make(chan int)
-        go GetPrimesAsync(1, 100, primes)
+        go GetPrimesAsync(1, 1000, primes)
     }
 }
