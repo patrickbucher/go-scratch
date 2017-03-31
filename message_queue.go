71c0a2f3b9f085386f6d5db7aa01a6b5c4ba9e8b
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2017-03-31 10:00:20 +0200

    modified message queue

--- message_queue.go
+++ message_queue.go
@@ -2,25 +2,29 @@ package main
 
 import "fmt"
 
-const consumers = 5
-const messages = 40
+const consumers = 10
+const messages = 50
 
-func consume(number int, channel <-chan string, consumed chan<- bool) {
-    for message := range channel {
-        fmt.Printf("consumer %d consumes message \"%s\"\n", number, message)
-    }
-    consumed<- true
+func consume(number int, queue <-chan string, consumed chan<- bool) {
+for message := range queue {
+fmt.Printf("consumer %2d consumes message \"%s\"\n", number, message)
+}
+consumed <- true
+}
+
+func produce(queue chan<- string) {
+	for i := 0; i < messages; i++ {
+		queue <- fmt.Sprintf("Yooo-Hooo! %2d", i+1)
+	}
+	close(queue)
 }
 
 func main() {
-    queue := make(chan string)
-    control := make(chan bool)
-    for i := 0; i < consumers; i++ {
-        go consume(i + 1, queue, control)
-    }
-    for i := 0; i < messages; i++ {
-        queue<- fmt.Sprintf("hello %2d", i + 1)
-    }
-    close(queue)
-    <-control
+	queue := make(chan string)
+	control := make(chan bool)
+	for i := 0; i < consumers; i++ {
+		go consume(i+1, queue, control)
+	}
+	go produce(queue)
+	<-control
 }
