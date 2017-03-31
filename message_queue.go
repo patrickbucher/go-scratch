996adcd313c8168dc85e1dcb476650f927edbcf4
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2017-03-31 13:10:25 +0200

    improved message_queue so that it has multiple producers (and works correctly)

--- message_queue.go
+++ message_queue.go
@@ -3,28 +3,38 @@ package main
 import "fmt"
 
 const consumers = 10
-const messages = 50
+const producers = 5
+const messages = 30
 
 func consume(number int, queue <-chan string, consumed chan<- bool) {
-for message := range queue {
-fmt.Printf("consumer %2d consumes message \"%s\"\n", number, message)
-}
-consumed <- true
+	for message := range queue {
+		fmt.Printf("consumer %2d consumes message \"%s\"\n", number, message)
+	}
+	consumed<- true
 }
 
-func produce(queue chan<- string) {
-	for i := 0; i < messages; i++ {
-		queue <- fmt.Sprintf("Yooo-Hooo! %2d", i+1)
+func produce(number int, queue chan<- string, produced chan<- bool) {
+	for i := 0; i < messages / producers; i++ {
+		queue <- fmt.Sprintf("Yooo-Hooo! %2d from producer %2d", i+1, number)
 	}
-	close(queue)
+    produced<- true
 }
 
 func main() {
 	queue := make(chan string)
-	control := make(chan bool)
+	producerControl := make(chan bool)
+	consumerControl := make(chan bool)
 	for i := 0; i < consumers; i++ {
-		go consume(i+1, queue, control)
+		go consume(i + 1, queue, consumerControl)
 	}
-	go produce(queue)
-	<-control
+    for i := 0; i < producers; i++ {
+        go produce(i + 1, queue, producerControl)
+    }
+    for i := 0; i < producers; i++ {
+        <-producerControl
+    }
+    close(queue)
+    for i := 0; i < consumers; i++ {
+        <-consumerControl
+    }
 }
