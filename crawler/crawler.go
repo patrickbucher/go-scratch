ddbd89b153afd79cb39a73573e0795351294b26f
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2017-12-26 22:57:31 +0100

    first working version of the page crawler; code to be improved

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"os"
 	"regexp"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -15,17 +17,11 @@ func main() {
 		os.Exit(1)
 	}
 	url := os.Args[1]
-	resp, err := http.Get(url)
+	content, err := getPageContent(url)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error fetching url %v: %v\n", url, err)
+		fmt.Fprintf(os.Stderr, "error fetching for url %v: %v\n", url, err)
 		os.Exit(1)
 	}
-	body, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error reading content: %v\n", err)
-	}
-	content := string(body)
 	r, _ := regexp.Compile(`href="[^;]*(;art[^"]+)"`)
 	hrefs := r.FindAllStringSubmatch(content, -1)
 	ids := make(map[string]struct{})
@@ -36,7 +32,42 @@ func main() {
 			ids[id] = struct{}{}
 		}
 	}
-	for k, _ := range ids {
-		fmt.Println(k)
+	now := time.Now()
+	subdir := now.Format("2006-01-02-15:04:05")
+	dir := strings.Join([]string{"dump", subdir}, string(os.PathSeparator))
+	err = os.MkdirAll(dir, 0755)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error creating dir %s: %v\n", dir, err)
+		os.Exit(1)
 	}
+	for k, _ := range ids {
+		artUrl := url + "/" + k
+		artContent, err := getPageContent(artUrl)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error downloading %s: %v\n", artUrl, err)
+		}
+		artFile := strings.Replace(k, ";", "", 1)
+		artFile = strings.Replace(artFile, ",", "-", 1)
+		artFilePath := dir + string(os.PathSeparator) + artFile
+		f, err := os.Create(artFilePath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error opening %s: %v\n", artFilePath, err)
+			continue
+		}
+		f.WriteString(artContent)
+		f.Sync()
+	}
+}
+
+func getPageContent(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
 }
