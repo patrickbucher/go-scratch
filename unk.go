39f26aee45e93789896ed527c93581514d4eb8e1
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2017-12-29 18:38:22 +0100

    added unk.go: a program to a specific problem posted on StackExchange

---
+++ unk.go
@@ -0,0 +1,39 @@
+// unk.go is a possible solution for a specific problem posted on StackExchange
+// https://unix.stackexchange.com/questions/413664/replace-string-in-a-huge-70gb-one-line-text-file
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	const (
+		pattern     = "<unk>"
+		replacement = "<raw_unk>"
+	)
+	var match int
+	var char rune
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanRunes)
+	for scanner.Scan() {
+		char = rune(scanner.Text()[0])
+		if char == []rune(pattern)[match] {
+			match++
+			if match == len(pattern) {
+				fmt.Print(replacement)
+				match = 0
+			}
+		} else {
+			if match > 0 {
+				fmt.Print(string(pattern[:match]))
+			}
+			fmt.Print(string(char))
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
