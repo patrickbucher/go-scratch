5985d6ee71cb2d05e3eb717b271f02546ebb967b
Patrick Bucher <patrick.bucher@peax.ch>
2018-01-24 12:34:23 +0100

    implemented optimized bubble sort; test and benchmarks for both bubble sorts

--- sorting/sorting_test.go
+++ sorting/sorting_test.go
@@ -2,6 +2,41 @@ package sorting
 
 import "testing"
 
-func TestBubbleSort(t *testing.T) {
+const (
+	testSize  = 10000   // numbers of items to be sorted
+	maxNumber = 1000000 // biggest item possible
+)
 
+func TestBubbleSort(t *testing.T) {
+	numbers := RandomIntSlice(testSize, maxNumber)
+	BubbleSort(numbers)
+	if ok := CheckSorted(numbers); !ok {
+		t.Fatalf("%v is not sorted", numbers)
+	}
+}
+
+func TestBubbleSortOptimized(t *testing.T) {
+	numbers := RandomIntSlice(testSize, maxNumber)
+	BubbleSortOptimized(numbers)
+	if ok := CheckSorted(numbers); !ok {
+		t.Fatalf("%v is not sorted", numbers)
+	}
+}
+
+func BenchmarkBubbleSort(b *testing.B) {
+	numbers := RandomIntSlice(testSize, maxNumber)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		BubbleSort(numbers)
+	}
+	b.StopTimer()
+}
+
+func BenchmarkBubbleSortOptimized(b *testing.B) {
+	numbers := RandomIntSlice(testSize, maxNumber)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		BubbleSortOptimized(numbers)
+	}
+	b.StopTimer()
 }
